8262ef538eefea58aee00242da45bb1fcb38f363
Nikita <shulgamalthael@gmail.com>
2020-07-17 20:48:28 +0300

    init files

---
+++ lesson-2/task1/index.js
@@ -0,0 +1,9 @@
+
+const user_Name = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDESSERT = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
