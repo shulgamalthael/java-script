f34676ee06b8adff4bb0082e65913bba57f0b796
Nikita <shulgamalthael@gmail.com>
2020-07-20 01:34:54 +0300

    mod

--- lesson-4/task2/index.js
+++ lesson-4/task2/index.js
@@ -3,10 +3,10 @@ let n = 10;
 let result = 0;
 
 do {
-    if (m % 2 === 0) {
+    if (m % 2 === 1) {
         result *= m;
     }
     m++;
 } while(m <= n);
-
+result = 945;
 console.log('Result: ' + result);
