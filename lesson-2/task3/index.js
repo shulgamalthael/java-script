b5ec2d91cc686fa67337bce067c11a1eaa1c3e60
Nikita <shulgamalthael@gmail.com>
2020-07-19 19:46:39 +0300

    init files

---
+++ lesson-2/task3/index.js
@@ -0,0 +1,13 @@
+let goodNumer = 17;
+let anotherNumber = goodNumber = 77;
+console.log(goodNumber);
+console.log(anotherNumber);
+
+let customer = {
+    name: 'John',
+    age: 17
+};
+let anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
