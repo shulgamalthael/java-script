2ca52a350525c70d1d9ecd85606d743917165ce3
Nikita <shulgamalthael@gmail.com>
2020-07-20 02:19:14 +0300

    mod

--- lesson-4/task2/index.js
+++ lesson-4/task2/index.js
@@ -1,12 +1,12 @@
 let m = 10;
 const n = 20;
-let result;
+let result = 1;
 
 do {
-    if (m % 2 === 1) {
+    if ( m%2 === 1) {
         result *= m;
     }
     m++;
-} while(m <= n);
+}while (m <= n);
 
-console.log('Result: ' + result);
+console.log('Result ' + result);
