3097eb9b386d217a8e431d8637546058dc5e8fcf
Nikita <shulgamalthael@gmail.com>
2020-07-19 19:57:36 +0300

    mod

--- lesson-2/task3/index.js
+++ lesson-2/task3/index.js
@@ -1,13 +1,14 @@
-let goodNumer = 17;
-let anotherNumber = goodNumber = 77;
+const goodNumber = 17;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
 console.log(goodNumber);
 console.log(anotherNumber);
 
-let customer = {
+const customer = {
     name: 'John',
     age: 17
 };
-let anotherCustomer = customer;
+const anotherCustomer = customer;
 anotherCustomer.age = 20;
 console.log(customer.age);
 console.log(anotherCustomer.age);
