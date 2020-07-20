fc6247e7f9a6a8d96e396a440b89bcea20f69090
Nikita <shulgamalthael@gmail.com>
2020-07-20 03:20:46 +0300

    init files

---
+++ lesson-4/hw2/index.js
@@ -0,0 +1,19 @@
+let m = 1;
+let n = 5;
+let result = 0;
+
+do {
+    if (m % 2 === 0 && m % 4 === 1){
+        result += m;
+    }
+    if (m % 3 === 0){
+        result -= m;
+    }
+    if (m % 4 === 0){
+        result *= m;
+    }
+    if (m % 5 === 0){
+        console.log(m); 
+    }
+    m++
+}while(m <= n);
