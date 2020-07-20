ed6234f939f49b3bdd1c902b698bcad33d551929
Nikita <shulgamalthael@gmail.com>
2020-07-20 03:47:40 +0300

    mod

--- lesson-4/hw3/index.js
+++ lesson-4/hw3/index.js
@@ -1,12 +1,11 @@
 let sum = 0;
-let summ;
 for ( let i = 0; i <= 1000; i++) {
     if ( i % 2 === 1) {
         console.log('Found');
-        summ = sum + i;
+        sum += i;
     }
 }
-if ( summ * 5 > 5000){
+if ( sum * 5 > 5000){
     console.log('Bigger');
 }
 else {
