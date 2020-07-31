7e6392a18d8b743a8fca890e55719b506825489a
Nikita <shulgamalthael@gmail.com>
2020-07-31 23:20:03 +0300

    mod

--- lesson-6/task3/index.js
+++ lesson-6/task3/index.js
@@ -1,7 +1,7 @@
 function getSpecialNumbers(m,n) {
     let getOddarr = array();
     for( let i = 0; i < array.length; i++) {
-        if ( array[i] % 3 === 0) {
+        if ( i % 3 === 0) {
             return array;
         }
         return array;
