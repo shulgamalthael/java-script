2feb8f3a04684a0971299456b67afe3a93a70d6e
Nikita <shulgamalthael@gmail.com>
2020-07-31 23:18:28 +0300

    init files

---
+++ lesson-6/task3/index.js
@@ -0,0 +1,9 @@
+function getSpecialNumbers(m,n) {
+    let getOddarr = array();
+    for( let i = 0; i < array.length; i++) {
+        if ( array[i] % 3 === 0) {
+            return array;
+        }
+        return array;
+    }
+}
