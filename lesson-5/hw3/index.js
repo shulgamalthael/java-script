bb6c8f31990da2f4aed8d81b621fcdda7dfa69d1
Nikita <shulgamalthael@gmail.com>
2020-07-31 15:34:50 +0300

    mod

--- lesson-5/hw3/index.js
+++ lesson-5/hw3/index.js
@@ -1,6 +1,6 @@
 function compareSums(a,b,c,d) {
     let firstSum = sum(a,b);
-    let secondSum = sum(c + d);
+    let secondSum = sum(c,d);
 
     if ( firstSum > secondSum) {
         return true;
