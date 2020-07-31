21baa0eaa72c63d5bae4bce7cbca1bf2c9eeda7a
Nikita <shulgamalthael@gmail.com>
2020-07-31 22:44:52 +0300

    mod

---
+++ lesson-6/task2/index.js
@@ -0,0 +1,12 @@
+function getSum(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    let sum = 0;
+    const arraySum = array();
+    for (let i = 0; i <= array.length; i ++) {
+        sum += i;
+    }
+    return sum;
+}
