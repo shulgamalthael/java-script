35352d42cd0f5e4107d4668bd66b5822eff40b95
Nikita <shulgamalthael@gmail.com>
2020-07-31 15:41:50 +0300

    mod

--- lesson-5/hw4/index.js
+++ lesson-5/hw4/index.js
@@ -1,8 +1,11 @@
 function findDivCount(a,b,n) {
     let result = 0;
-    for( let i = a; i < b; i++) {
+    for( let i = a; i <= b; i++) {
         if ( i % n === 0) {
             result += 1;
+        } else {
+            return 0;
         }
+        return result;
     }
 }
