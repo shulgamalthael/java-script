8ecafa9853ccca42717c598b3ec1b6ae68bd9ddb
Nikita <shulgamalthael@gmail.com>
2020-07-31 13:28:20 +0300

    mod

--- lesson-4/hw2/index.js
+++ lesson-4/hw2/index.js
@@ -1,19 +1,19 @@
-let m = 1;
+let m = 30;
 let n = 50;
+
 let result = 0;
 
-do {
-    if (m % 2 === 0 && m % 4 === 1){
-        result += m;
+for (let i = m; i <= n; i++) {
+
+    if (i % 2 === 0 && i % 4 !== 0) {
+        result += i;
+    } else if (i % 3 === 0) {
+        result -= i;
+    } else if (i % 4 === 0) {
+        result *= i;
     }
-    if (m % 3 === 0){
-        result -= m;
+
+    if (i % 5 === 0) {
+        console.log(i);
     }
-    if (m % 4 === 0){
-        result *= m;
-    }
-    if (m % 5 === 0){
-        console.log(m); 
-    }
-    m++
-}while(m <= n);
+}
