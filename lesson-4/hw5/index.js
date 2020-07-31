5bb98223c893de5d5f69479939a4c7aaf665a8e4
Nikita <shulgamalthael@gmail.com>
2020-07-31 14:13:35 +0300

    mod

--- lesson-4/hw5/index.js
+++ lesson-4/hw5/index.js
@@ -1,11 +1,11 @@
-const n = 1000;
-let sum = 0;
 
-for( let i = 1; i <= 1000; i++) {
-    sum += i;
+let task = 0;
+
+for (let i = 1; i > 1000; i++) {
+    task + i;
 }
 
-let summ = sum % 1234;
-let summm = Math.floor(sum / 1234);
-
-summ > summm ? console.log(true) : console.log(false);
+let num = Math.floor(task / 1234);
+let numWithout = num * 1234;
+let result = task - numWithout;
+num > result ? console.log(true) : console.log(false);
