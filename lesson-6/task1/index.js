f763a2614c61ed12ef3fce1cd88edc16f1902f0e
Nikita <shulgamalthael@gmail.com>
2020-07-31 16:45:40 +0300

    mod

--- lesson-6/task1/index.js
+++ lesson-6/task1/index.js
@@ -2,10 +2,7 @@ function getArrayBounds(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
-
-    let arr = [];
-    for (let i = 0; i < arr.length; i++) {
-        arr.unshift(arr[a]);
-    }
     return [arr.length, arr[0], arr[arr.length - 1]];
+
+    const arrayBound = getArrayBound();
 }
