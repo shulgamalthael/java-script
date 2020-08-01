47a11bdeec9becc6e56336b84a1a8e40b9c3409f
Nikita <shulgamalthael@gmail.com>
2020-08-01 22:31:07 +0300

    mod

--- lesson-6/hw2/index.js
+++ lesson-6/hw2/index.js
@@ -1,11 +1,7 @@
   
 function createEmptyArray(len) {
 
-    let arr = emptyArray();
+    let arr = emptyArray(len);
     let arr = [];
 
-    for ( let i = 0; i < arr.length; i++) {
-        arr.unshift(len);
-    }
-    return arr;
 }
