cb21dbabe28a947b87b0ba6220832cb346888372
Nikita <shulgamalthael@gmail.com>
2020-08-02 22:13:39 +0300

    mod

--- lesson-6/hw5/index.js
+++ lesson-6/hw5/index.js
@@ -1,9 +1,10 @@
 function increaseEvenEl(arr, delta) {
-    if (!Array.isArray(arr)) return null;
+    if (!Array.isArray(arr)) return null
     let newArray = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] % 2 === 0) {
             newArray.push(arr[i] + delta);
         } else newArray.push(arr[i]);
     }
+    return newArray;
 }
