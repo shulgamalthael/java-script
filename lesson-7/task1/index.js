8fe9241b5323345bfd7ec9257da6ed98b0f28866
Nikita <shulgamalthael@gmail.com>
2020-08-03 00:20:17 +0300

    mod

---
+++ lesson-7/task1/index.js
@@ -0,0 +1,11 @@
+const getSpecialNumbers = numbers => {
+    let specialNumbers = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 3 === 0) {
+            specialNumbers.push(numbers[i]);
+        }
+    }
+
+    return specialNumbers;
+};
