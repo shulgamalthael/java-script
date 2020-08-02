7718668fc7e903d39ffdf5fe6f1db9f7f196d780
Nikita <shulgamalthael@gmail.com>
2020-08-02 21:28:44 +0300

    init files

---
+++ lesson-6/hw3/index.js
@@ -0,0 +1,11 @@
+function squareArray(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+
+    let array = [];
+    for ( let i = 0; i < array.length; i++) {
+        return array[i] * array[i];
+    }
+    return array();
+}
