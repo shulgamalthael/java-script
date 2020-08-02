f12261642a66847d97753da3276268fb409c7d61
Nikita <shulgamalthael@gmail.com>
2020-08-02 23:15:47 +0300

    mod

--- lesson-6/hw12/index.js
+++ lesson-6/hw12/index.js
@@ -1,7 +1,6 @@
 function includes(arr,n) {
-    let newArray = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr.length[i] == n) return true;
+        if (arr[i] == n) return true;
     }
     return false;
 }
