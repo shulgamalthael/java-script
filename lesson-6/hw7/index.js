9cbf0033139b618f5c3dd5066bf321a056a9a296
Nikita <shulgamalthael@gmail.com>
2020-08-02 22:42:23 +0300

    mod

--- lesson-6/hw7/index.js
+++ lesson-6/hw7/index.js
@@ -1,9 +1,8 @@
 function cloneArr(arr) {
-    if(!Array.isArray(arr)) return null;
-
-    let cloneArr = []
-    for (let i = 0; i < arr.length - 1; i++) {
-        cloneArr.push(arr[i]);
+    if (!Array.isArray(arr)) return null;
+    let newArr = []
+    for (let i = 0; i <= arr.length - 1; i++) {
+        newArr.push(arr[i]);
     }
-    return cloneArr;
+    return newArr;
 }
