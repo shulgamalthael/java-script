0d83430a8ad4cdc5529a02dd2e3a56b138c25002
Nikita <shulgamalthael@gmail.com>
2020-08-03 01:05:06 +0300

    mod

--- lesson-7/task2/index.js
+++ lesson-7/task2/index.js
@@ -1,7 +1,7 @@
 const sortDesc = numbers => {
-    numbers.sort((a, b) => a - b);
+    let sortedNumbers = numbers.sort((a, b) => a - b);
 
-    return numbers;
+    return sortedNumbers;
 };
 
 const arr = [2, 4, 30, 1, 5, 21, 18];
