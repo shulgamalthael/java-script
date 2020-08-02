6f137ad5084e6a184daf11436f9f0074172db9a6
Nikita <shulgamalthael@gmail.com>
2020-08-02 21:57:37 +0300

    mod

--- lesson-6/hw4/index.js
+++ lesson-6/hw4/index.js
@@ -2,9 +2,8 @@ function checkSum(arr) {
     if(!Array.isArray(arr)) return null;
 
     sum = 0;
-    let checkSum = [];
-    for (let i = 0; i < checkSum.length; i++) {
-        sum = sum += checkSum[i];
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum += arr[i];
     }
 
     return (sum > 100);
