03f7265174e84cffde7ab32ca420e909ddc648d3
Nikita <shulgamalthael@gmail.com>
2020-08-03 02:08:17 +0300

    init files

---
+++ lesson-7/task4/index.js
@@ -0,0 +1,9 @@
+const getMessegesForPassedStudents = (allStudents, failedStudents) => {
+    const passedStudents = allStudents
+    .filter(name => !failedStudents.includes(name));
+
+    const massages = passedStudents
+    .map(name => 'Good job, ' + name);
+
+    return messages;
+};
