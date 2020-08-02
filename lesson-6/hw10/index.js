2993a1053d62a76ba74b87b19001f28ab0061768
Nikita <shulgamalthael@gmail.com>
2020-08-02 23:03:01 +0300

    init files

---
+++ lesson-6/hw10/index.js
@@ -0,0 +1,10 @@
+function withdraw(clients, balances, client, amount) {
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i] == client) {
+            if (balances[i] - amount < 0) {
+            return -1;
+        }
+        return balances[i] - amount;
+            }
+    }
+}
