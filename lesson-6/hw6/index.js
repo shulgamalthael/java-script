753727f9a7683a64aa1fa2700bbc3f8d6bc97a33
Nikita <shulgamalthael@gmail.com>
2020-08-02 22:27:04 +0300

    mod

--- lesson-6/hw6/index.js
+++ lesson-6/hw6/index.js
@@ -1,8 +1,7 @@
 function reverseArray(arr) {
     if (!Array.isArray(arr)) return null;
-
     let swapArr = []
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i <= arr.length; i++) {
         swapArr.push(arr[arr.length - i]);
     }
     return swapArr;
