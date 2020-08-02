3ef5d815ec679a46f04d7f8713977f8c362cb1d4
Nikita <shulgamalthael@gmail.com>
2020-08-02 23:28:29 +0300

    mod

--- lesson-6/hw13/index.js
+++ lesson-6/hw13/index.js
@@ -1,19 +1,17 @@
-function removeDuplicates(array) {
-    if (!Array.isArray(arr)) return null;
-    const unique = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (includes(unique, arr[i]) {
-            unique.push(arr[i]);
-        }
-    }
-    return unique;
-}
-
-function includes(arr, num) {
+function includes (arr, num) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === num) {
             return false;
         }
     }
     return true;
+}
+function removeDuplicates(arr) {
+    const unique = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (includes(unique, arr[i])) {
+            unique.push(arr[i]);
+        }
+    }
+    return unique;
 }
