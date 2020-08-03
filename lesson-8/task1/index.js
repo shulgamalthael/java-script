60bdafdb5d61ad0d6c23607c64969b28129a1d1d
Nikita <shulgamalthael@gmail.com>
2020-08-03 15:50:20 +0300

    init files

---
+++ lesson-8/task1/index.js
@@ -0,0 +1,11 @@
+const transformToObject = arr => {
+    let obj = {};
+
+    arr.forEach(el => obj[el] = el);
+
+    return obj;
+};
+
+const arr = [1, 'text']
+const result = transformToObject(arr);
+console.log(result);
