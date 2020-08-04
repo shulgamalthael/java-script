f1d7752559d0fecd96be8cf350c4a33827d60831
Nikita <shulgamalthael@gmail.com>
2020-08-04 05:17:54 +0300

    init files

---
+++ lesson-8/hw4/index.js
@@ -0,0 +1,26 @@
+function addPropertyV1(userData, userId) {
+    userData.id = userId;
+    return userData;
+};
+
+function addPropertyV2(userData, userId) {
+    const sourceData = { id: userId };
+    return Object.assign(userData, sourceData);
+};
+
+function addPropertyV3(userData, userId) {
+    const sourceData = { id: userId };
+    return Object.assign({}, sourceData, userData);
+};
+
+function addPropertyV4(userData, userId) {
+    let sorceData = { id: userId };
+    return Object.assign({...userData }, sorceData);
+};
+
+const user = {
+    name: 'Add',
+}
+const result = addPropertyV4(user, '3256897414');
+
+console.log(result);
