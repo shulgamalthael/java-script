2aeef044f38171bce5207d609a85da52bcd7bad6
Nikita <shulgamalthael@gmail.com>
2020-08-04 05:22:33 +0300

    mod

--- lesson-8/hw4/index.js
+++ lesson-8/hw4/index.js
@@ -1,26 +1,30 @@
-function addPropertyV1(userData, userId) {
-    userData.id = userId;
-    return userData;
-};
-
-function addPropertyV2(userData, userId) {
-    const sourceData = { id: userId };
-    return Object.assign(userData, sourceData);
-};
-
-function addPropertyV3(userData, userId) {
-    const sourceData = { id: userId };
-    return Object.assign({}, sourceData, userData);
-};
-
-function addPropertyV4(userData, userId) {
-    let sorceData = { id: userId };
-    return Object.assign({...userData }, sorceData);
-};
-
-const user = {
-    name: 'Add',
+const addPropertyV1 = (obj, key, value) => {
+    obj[key] = value;
+    return obj;
 }
-const result = addPropertyV4(user, '3256897414');
+const addPropertyV2 = (obj, key, value) => {
+    const sourceObj = {
+        [key]: value
+    };
+    return Object.assign(obj, sourceObj);
+}
+const addPropertyV3 = (obj, key, value) => {
+    const sourceObj = {
+        [key]: value
+    };
+    return Object.assign({}, obj, sourceObj);
+}
+const addPropertyV4 = (obj, key, value) => {
+    const sourceObj = {
+        [key]: value
+    };
+    return {...obj, ...sourceObj };
+}
+
+
+const transaction = {
+    value: 170,
+}
+const result = addPropertyV4(transaction, 'currency', 'USD');
 
 console.log(result);
