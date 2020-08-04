68f38afa63d42b2f15cbba0ead6c57f82c5f939e
Nikita <shulgamalthael@gmail.com>
2020-08-04 05:31:46 +0300

    mod

--- lesson-8/hw5/index.js
+++ lesson-8/hw5/index.js
@@ -1,33 +1,42 @@
 const mergeObjectsV1 = (obj1, obj2) => {
-    let resultedObj = Object.assign({}, obj1, obj2);
-    return resultedObj;
+    const result = Object.assign({}, obj1, obj2);
+    return result;
 };
 
 const mergeObjectsV2 = (obj1, obj2) => {
-    let resultedObj = Object.assign({}, obj2, obj1);
-    return resultedObj;
+    const result = Object.assign({}, obj2, obj1);
+    return result;
 };
 
 const mergeObjectsV3 = (obj1, obj2) => {
-    let resultedObj = {...obj1, ...obj2 }
-    return resultedObj;
-};
-const mergeObjectsV4 = (obj1, obj2) => {
-    let resultedObj = {...obj2, ...obj1 }
-    return resultedObj;
+    const result = {...obj1, ...obj2};
+    return result;
 };
 
-const obj1 = {
-    name: 'Tom',
+const mergeObjectsV4 = (obj1, obj2) => {
+    const result = {...obj2, ...obj1};
+    return result;
+};
+
+// const mergeObjectsV4 = (obj1, obj2) => {
+//     let clonedObj1 = {...obj1};
+//     let clonedObj2 = {...obj2};
+//     const result = Object.assign({}, clonedObj2, clonedObj1);
+
+//     return result;
+// };
+
+const user1 = {
+    name: 'Bob',
     age: 17,
 };
-const obj2 = {
-    name: 'Bob',
+
+const user2 = {
+    name: 'Patrick',
     student: false,
 };
 
-
-console.log(mergeObjectsV1(obj1, obj2));
-console.log(mergeObjectsV2(obj1, obj2));
-console.log(mergeObjectsV3(obj1, obj2));
-console.log(mergeObjectsV4(obj1, obj2));
+// console.log(mergeObjectsV1(user1, user2));
+// console.log(mergeObjectsV2(user1, user2));
+// console.log(mergeObjectsV3(user1, user2));
+// console.log(mergeObjectsV4(user1, user2));
