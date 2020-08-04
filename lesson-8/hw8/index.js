974ea565999eab57908af8bacad9b7987f02f0da
Nikita <shulgamalthael@gmail.com>
2020-08-04 12:42:31 +0300

    mod

--- lesson-8/hw8/index.js
+++ lesson-8/hw8/index.js
@@ -1,28 +1,21 @@
-const addPropertyV1 = (obj, key, value) => {
-    obj[key] = value;
-    return obj;
-};
+obj[key] = value;
+return obj;
+}
 
 const addPropertyV2 = (obj, key, value) => {
-    const result = Object.assign(obj, {[key]: value});
-    return result;
-};
+
+Object.assign(obj,{[key]: value});
+return obj;
+}
 
 const addPropertyV3 = (obj, key, value) => {
-    const result = Object.assign({}, obj,  {[key]: value});
-    return result;
-};
-
+let newObj = {};
+Object.assign(newObj, obj, {[key]: value});
+return newObj;
+}
 const addPropertyV4 = (obj, key, value) => {
-    const result = Object.assign({...obj}, {[key]: value});
-    return result;
-};
 
-const user = {
-    lastName: 'King',
-};
-
-// console.log(addPropertyV1(user, 'firstName', 'Stephen'));
-// console.log(addPropertyV2(user, 'firstName', 'Stephen'));
-// console.log(addPropertyV3(user, 'firstName', 'Stephen'));
-// console.log(addPropertyV4(user, 'firstName', 'Stephen'));
+let newObj = {...obj};
+newObj[key] = value;
+return newObj;
+}
