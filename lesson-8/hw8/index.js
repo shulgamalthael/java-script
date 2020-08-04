9d22052bff4ec73f714136a5d2b9db948ddcb0ba
Nikita <shulgamalthael@gmail.com>
2020-08-04 12:56:28 +0300

    mod

--- lesson-8/hw8/index.js
+++ lesson-8/hw8/index.js
@@ -1,43 +1,23 @@
-const obj = {
+  
+const addPropertyV1 = (userData, userId) => {
+    user.id = userId;
+    return userData;
 };
 
-const key = 'key';
-const value = 'value';
-
-const userId = 'asd';
-
-function addPropertyV1 (obj, key, value) {
-  obj[key] = value;
-  return obj;
+function addPropertyV2(userData, userId) {
+    const sourceData = { id: userId };
+    return Object.assign(userData, sourceData);
+};
+function addPropertyV3(userData, userId) {
+    const sourceData = { id: userId };
+    return Object.assign({}, sourceData, userData);
+};
+function addPropertyV4(userData, userId) {
+    let sorceData = { id: userId };
+    return Object.assign({...userData }, sorceData);
+};
+const user = {
+    name: 'Add',
 }
-
-function addPropertyV2 (obj, key, value) {
-  obj[key] = value;
-  const newObj = Object.assign({}, obj);
-
-  return newObj;
-}
-
-const addPropertyV3 = (obj, key, value) => {
-  const newObj = Object.assign({}, obj);
-  newObj[key] = value;
-
-  return newObj;
-};
-
-const addPropertyV4 = (obj, key, value) => {
-  const newObj = {...obj };
-  newObj[key] = value;
-
-  return newObj;
-};
-
-addPropertyV1 (obj, key, value);
-console.log(obj);
-addPropertyV2 (obj, key, value);
-console.log(obj);
-addPropertyV3 (obj, key, value);
-console.log(obj);
-addPropertyV4 (obj, key, value);
-console.log(obj);
-© 2020 GitHub, Inc.
+const result = addPropertyV4(user, '3256897414');
+console.log(result);
