22c241587ec5f6116d37cbf72fbe8d323d95b59a
Nikita <shulgamalthael@gmail.com>
2020-08-05 01:51:46 +0300

    mod

--- lesson-9/hw1/index.js
+++ lesson-9/hw1/index.js
@@ -1,29 +1,36 @@
 const rooms = {
     room1: [
-        {name: 'room1 name1'},
-        {name: 'room1 name2'},
-        {name: 'room1 name3'},
-        {name: 'room1 name4'},
+        { name: 'room1 name1' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
     ],
     room2: [
-        {name: 'room2 name1'},
-        {name: 'room2 name3'},
-        {name: 'room2 name2'},
-        {name: 'room2 name4'},
+        { name: 'room2 name1' },
+        { name: 'room2 name2' },
+        { name: 'room2 name3' },
+        { name: 'room2 name4' },
     ],
     room3: [
-        {name: 'room3 name1'},
-        {name: 'room3 name2'},
-        {name: 'room3 name3'},
-        {name: 'room3 name4'},
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+        { name: 'room3 name4' },
     ],
-};
+    room4: [
+        { name: 'room4 name1' },
+        { name: 'room4 name2' },
+        { name: 'room4 name3' },
+        { name: 'room4 name4' },
+    ],
 
-const getPeople = object => Object.values(object).flat().map(elem => (elem = elem.name))
-return getPeople;
+}
 
-const usersArr = getPeople(rooms);
+const getPeople = obj => Object.values(obj).flat().map(obj => Object.values(obj)).flat();
+// cloneObj = {...obj };
+// const middleObj = Object.values(cloneObj).flat().map(obj => Object.values(obj)).flat()
 
-console.log(usersArr);
 
+const result = getPeople(rooms);
+console.log(result);
 console.log(rooms);
