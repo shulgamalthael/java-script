d5b5a0bdf080e322a6880e997b017a959a20b1d9
Nikita <shulgamalthael@gmail.com>
2020-08-06 02:45:45 +0300

    mod

--- lesson-9/hw2/index.js
+++ lesson-9/hw2/index.js
@@ -1,11 +1,19 @@
-const users = {
-    'Tom': 18,
-    'Donna': 22,
-    'Jack': 15,
+const customers = {
+    "customer-id-1": {
+        name: "William",
+        age: 54
+    },
+    "customer-id-2": {
+        name: "Tom",
+        age: 17
+    }
 };
 
-const getAdults = userObj => Object.entries(userObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
+const getCustomersList = obj => Object.entries(obj)
+    .map(elem => ({ id: elem[0], ...obj[elem[0]] }))
+    .sort((a, b) => a.age - b.age);
 
-console.log(getAdults(users));
+// const getCustomersList = obj => Object.entries(obj).forEach([key, value]);
+
+console.log(getCustomersList(customers));
+console.log(Object.values(customers));
