3b68545316f5ef6d3803138b20f64099a02c0695
Nikita <shulgamalthael@gmail.com>
2020-08-06 13:35:53 +0300

    mod

--- lesson-9/hw3/index.js
+++ lesson-9/hw3/index.js
@@ -1,9 +1,8 @@
-const dayTransactions = [
-    { userid: 22, amount: 60, operation: 'sell'},
-    { userid: 22, amount: 160, operation: 'buy'},
-    { userid: 44, amount: 90, operation: 'sell'},
+const dayTransaction = [
+    { userId: 22, amount: 55, operation: 'sell' },
+    { userId: 38, amount: 210, operation: 'sell' },
+    { userId: 274, amount: 112, operation: 'sell' },
 ];
-
 function getTotalRevenue(dayTransaction) {
       return dayTransaction.map((el) => el.amount).reduce((a, b) => a + b, 0);
 }console.log(getTotalRevenue(dayTransaction));
