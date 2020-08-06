9898863c3914475a24f532982a699fe3157a4a9d
Nikita <shulgamalthael@gmail.com>
2020-08-06 14:40:55 +0300

    mod

--- lesson-10/task2/index.js
+++ lesson-10/task2/index.js
@@ -4,15 +4,16 @@ const getParsedIntegers = arr => arr
 const getParsedIntegersV2 = arr => arr
     .map(elem => parseInt(elem));
 
-const getParsedFloat = arr => arr
+const getParsedFloats = arr => arr
     .map(elem => Number.parseFloat(elem));
 
-const getParsedFloatV2 = arr => arr
+const getParsedFloatsV2 = arr => arr
     .map(elem => parseFloat(elem));
 
-    const numberList = [77, '39', '45.9txt', Infinity, null, NaN, undefined];
+
+const numberList = [77, '39', '45.9txt', Infinity, null, NaN, undefined];
 
 console.log(getParsedIntegers(numberList));
 console.log(getParsedIntegersV2(numberList));
-console.log(getParsedFloat(numberList));
-console.log(getParsedFloatV2(numberList));
+console.log(getParsedFloats(numberList));
+console.log(getParsedFloatsV2(numberList));
