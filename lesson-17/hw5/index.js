89c15fb5445e981ea807d53462d203f96f79d5b4
Nikita <shulgamalthael@gmail.com>
2020-08-11 18:31:34 +0300

    mod

--- lesson-17/hw5/index.js
+++ lesson-17/hw5/index.js
@@ -1,41 +1,36 @@
-const timer = {
-    secondsPassed: 0,
+const formatSeconds = seconds => {
+    if (seconds < 10) {
+        return `0${seconds}`
+    }
+    return seconds;
+};
+export const timer = {
     minsPassed: 0,
-    starter: 0,
+    secondsPassed: 0,
+    _interval: null,
     startTimer() {
-        // console.log(setInterval(this.secondsPassed, 5000));
-        // alert(setInterval(this.secondsPassed === 5000));
-
-        this.starter = setInterval(() => {
-            if (this.secondsPassed === 60) {
-                this.secondsPassed = 0;
-                this.minsPassed++;
-            }
-            // console.log(this.secondsPassed)
-            // console.log(this.minsPassed)
+        this._interval = setInterval(() => {
             this.secondsPassed += 5;
+            if (this.secondsPassed === 60) {
+                this.minsPassed += 1;
+                this.secondsPassed = 0;
+            }
         }, 5000);
     },
-
-    getTime() {
-
-        if (this.secondsPassed >= 10) {
-            console.log(`${this.minsPassed}:${this.secondsPassed}`);
-            // return `${this.minsPassed}:${this.secondsPassed}`;
-        } else {
-            console.log(`${this.minsPassed}:0${this.secondsPassed}`);
-            // return `${this.minsPassed}:${this.secondsPassed + '0'}`;
-        }
-    },
-
     stopTimer() {
-         console.log(clearInterval(this.startTimer()));
-        clearInterval(this.starter);
+        clearInterval(this._interval);
+        this._interval = null;
     },
-
-    resetTime() {
-        // console.log(this.secondsPassed)
-        this.secondsPassed = 0;
+    resetTimer() {
+        this.stopTimer();
         this.minsPassed = 0;
+        this.secondsPassed = 0;
+    },
+    getTime() {
+        const formattedSeconds = formatSeconds(this.secondsPassed);
+        return `${this.minsPassed}:${formattedSeconds}`;
     }
 };
+
+// timer.startTimer();
+// timer.getTime();
