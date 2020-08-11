cf5535885d5d1f1a3a06ada877fd9bf7c0cac431
Nikita <shulgamalthael@gmail.com>
2020-08-11 12:20:41 +0300

    init files

---
+++ lesson16/hw1/index.js
@@ -0,0 +1,15 @@
+export const createArrayOfFunctions = (num) => {
+    let newArr = [];
+    if (num === undefined ) return newArr;
+    if (typeof(num) == 'number') return null;
+   
+    for (let i = 0; i < num; i++) {
+        newArr[i] = () => {
+            return i;
+        };
+    }
+    return newArr;
+}
+
+createArrayOfFunctions(9);
+console.log(createArrayOfFunctions(7));
