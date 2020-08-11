41d90b158ea9dc4c35c2df0b10c8fb0a817549dc
Nikita <shulgamalthael@gmail.com>
2020-08-11 11:57:13 +0300

    mod

--- lesson16/task2/index.js
+++ lesson16/task2/index.js
@@ -1,9 +1,12 @@
-var arr = [];
+  
+let arr = [];
 
 for(let i = 0; i < 10; i++) {
-        arr[i] = function() {
-            return i;
-        }
+    arr[i] = function() {
+        return i;
     }
+}
 
-console.log(arr[1]());
+// console.log(arr[2]());
+
+export { arr };
