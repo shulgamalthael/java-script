ac8dae15f0d3a151eca8b0d40be50c953570df81
Nikita <shulgamalthael@gmail.com>
2020-08-12 17:50:57 +0300

    init files

---
+++ lesson-19/task4/index.js
@@ -0,0 +1,17 @@
+const user = {
+    firstName: 'Bruce',
+    lastName: 'Wayne',
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+        // console.log(`${this.firstName} ${this.lastName}`);
+    },
+    set fullName(value) {
+        const [firstName, lastName] = value.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    },
+};
+
+user.fullName();
+
+export default user;
