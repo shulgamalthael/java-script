3cd0c736713b2c06c7dd02ccddac23017a5826d4
Nikita <shulgamalthael@gmail.com>
2020-08-12 16:08:00 +0300

    init files

---
+++ lesson-19/task1/index.js
@@ -0,0 +1,29 @@
+// vehicle and ship
+
+const vehicle = {
+    name: 'Argo',
+    move() {
+        console.log(`${this.name}` + ' is moving');
+    },
+    stop() {
+        console.log(`${this.name}` + ' stopped');
+    },
+};
+
+const ship = {
+    startMachine() {
+        console.log(`${this.name}` + ' lifting anchor up');
+    },
+    stopMachine() {
+        console.log(`${this.name}` + ' lifting anchor down');
+    },
+    __proto__: vehicle,
+    
+}
+
+// vehicle.move();
+// vehicle.stop();
+// console.log(vehicle);
+// ship.startMachine();
+// ship.stopMachine();
+// console.log(ship);
