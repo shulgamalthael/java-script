7dbd3703e4440441be88a395a47d70aca1530fd8
Nikita <shulgamalthael@gmail.com>
2020-08-12 16:12:45 +0300

    mod

--- lesson-19/task1/index.js
+++ lesson-19/task1/index.js
@@ -3,19 +3,19 @@
 const vehicle = {
     name: 'Argo',
     move() {
-        console.log(`${this.name}` + ' is moving');
+        console.log(`${this.name} is moving`);
     },
     stop() {
-        console.log(`${this.name}` + ' stopped');
+        console.log(`${this.name} stopped`);
     },
 };
 
 const ship = {
     startMachine() {
-        console.log(`${this.name}` + ' lifting anchor up');
+        console.log(`${this.name} lifting anchor up`);
     },
     stopMachine() {
-        console.log(`${this.name}` + ' lifting anchor down');
+        console.log(`${this.name} lifting anchor down`);
     },
     __proto__: vehicle,
     
