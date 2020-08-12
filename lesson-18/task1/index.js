f93b4aa3dcc96d5965eda17538bec800341f50d3
Nikita <shulgamalthael@gmail.com>
2020-08-12 14:13:46 +0300

    mod

--- lesson-18/task1/index.js
+++ lesson-18/task1/index.js
@@ -1,21 +1,23 @@
-'use strict';
+// 'use strict';
 
-const event= {
+const event = {
     guests: [
-        { name: 'Alex', email: 'exemple@server.com', age: 17 },
-        { name: 'Tom', email: 'exemple@server.com', age: 18 },
-        { name: 'John', email: 'exemple@server.com', age: 18 },
+        { name: 'Tom', age: 17, email: 'example@Server.com', },
+        { name: 'Bob', age: 18, email: 'example@Server.com', },
+        { name: 'Sam', age: 22, email: 'example@Server.com', },
     ],
-    message: 'Welcome to the party',
-    getInvintations() {
+    message: "Welcome to the party!",
+    getInvitations() {
         return this.guests
-
         .filter(({ age }) => age >= 18)
         .map(({ name, email }) => ({
-            text: `Dear ${name}!${this.message}!`,
+            email: `${email} `,
+            message: `Dear ${name}! ${this.message}`,
             email
         }));
     }
-};
+}
 
-console.log(event.getInvintations());
+console.log(event.getInvitations());
+
+export { event };
