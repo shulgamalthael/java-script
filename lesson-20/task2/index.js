765bd06522006bfeabfc121bf4f283c5aaecb8c0
Nikita <shulgamalthael@gmail.com>
2020-08-13 13:30:12 +0300

    init files

---
+++ lesson-20/task2/index.js
@@ -0,0 +1,37 @@
+class Vehicle {
+    constructor(name, numberOfWheels) {
+        this.name = name;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+    move() {
+        console.log(`${this.name} is moving`);
+    }
+
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+};
+
+class Ship extends Vehicle {
+    constructor(name, numberOfWheels, maxSpeed) {
+        super(name, numberOfWheels);
+        this.maxSpeed = maxSpeed;
+    }
+
+    move() {
+        console.log(`${this.name} lifting anchor up`);
+        super.move();
+    }
+
+    stop() {
+        super.stop();
+        console.log(`${this.name} lifting anchor down`);
+    }
+};
+
+// const ship1 = new Ship('Argo');
+
+// ship1.move();
+
+export { Vehicle, Ship };
