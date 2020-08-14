f098257dac62490e6f52e42fa3c0c2235774ac4b
Nikita <shulgamalthael@gmail.com>
2020-08-14 14:11:08 +0300

    mod

--- lesson-21/hw12/index.js
+++ lesson-21/hw12/index.js
@@ -1,20 +1,7 @@
-function getSection(num) {
-    const findNumberSec = document.querySelector(`span[data-number="${num}"]`);
+function getSection() {
+    const span = document.querySelector(`span[data-number=${num}]`);
     const getTag = findNumberSec.closest('div');
     const getDataSection = getTag.getAttribute('data-section');
-    // console.log(findNumberSec)
-    // console.log(getTag)
-    // console.log(getDataSection)
-    // findNumberSec.forEach(function(item) {
-    //     console.log(item.dataset.number)
-    //     if (item.dataset.number === num) {
-    //         console.log(item)
-    //     }
-    // })
+
     return getDataSection;
-}
-
-// getSection(5);
-
-export { getSection };
-© 2020 GitHub, Inc.
+};
