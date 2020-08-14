90cc8e4c16466ae9c7262165d49e68f03dd16a01
Nikita <shulgamalthael@gmail.com>
2020-08-14 12:19:35 +0300

    mod

--- lesson-21/hw3/index.js
+++ lesson-21/hw3/index.js
@@ -1,17 +1,17 @@
 function getTitle() {
-    const title = document.querySelector('.title').textContent;
+    return title = document.querySelector('.title').textContent;
 }
 
 function getDescription() {
-    const description = document.querySelector('.about').innerText;
+    return description = document.querySelector('.about').innerText;
 }
 
 function getPlans() {
-    const plans = document.querySelector('.plans').innerHTML;
+    return plans = document.querySelector('.plans').innerHTML;
 }
 
 function goal() {
-    const goal = document.querySelector('.goal').outerHTML;
+    return goal = document.querySelector('.goal').outerHTML;
 }
 
 export {getTitle, getDescription, getPlans, goal};
