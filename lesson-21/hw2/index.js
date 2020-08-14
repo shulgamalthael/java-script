9d2574ea7cf401e1e3ed7ebe5c3eb1db17da5e22
Nikita <shulgamalthael@gmail.com>
2020-08-14 12:00:05 +0300

    init files

---
+++ lesson-21/hw2/index.js
@@ -0,0 +1,13 @@
+const getItemsList = () => {
+    const itemList =  document.querySelectorAll('technology');
+    return itemList;
+    console.dir(itemList);
+};
+
+const getItemsArray = () => {
+    const itemsArray = document.querySelectorAll('tool');
+    const arrayElems = [];
+    arrayElems.map(elem => elem);
+    return arrayElems;
+    console.dir()
+};
