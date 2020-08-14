ad7a399c25efc59de0c0f7120028553dd7a2e938
Nikita <shulgamalthael@gmail.com>
2020-08-14 11:52:38 +0300

    init files

---
+++ lesson-21/hw1/index.js
@@ -0,0 +1,15 @@
+export const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+    return titleElem;
+};
+
+getTitleElement();
+
+export const getInputElement = () => {
+    const inputElem = document.querySelector('input[type="text"]');
+    console.dir(inputElem);
+    return inputElem;
+};
+
+getInputElement();
