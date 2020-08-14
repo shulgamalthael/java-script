7c280e790b896d7a4495be6457423f864ca7e654
Nikita <shulgamalthael@gmail.com>
2020-08-14 14:14:33 +0300

    mod

--- lesson-21/hw12/index.js
+++ lesson-21/hw12/index.js
@@ -1,9 +1,19 @@
-function getSection() {
-    const span = document.querySelector(`span[data-number=${num}]`);
+function getSection(num) {
+    const findNumberSec = document.querySelector(`span[data-number="${num}"]`);
     const getTag = findNumberSec.closest('div');
     const getDataSection = getTag.getAttribute('data-section');
-
+    // console.log(findNumberSec)
+    // console.log(getTag)
+    // console.log(getDataSection)
+    // findNumberSec.forEach(function(item) {
+    //     console.log(item.dataset.number)
+    //     if (item.dataset.number === num) {
+    //         console.log(item)
+    //     }
+    // })
     return getDataSection;
-};
+}
+
+// getSection(5);
 
 export { getSection };
