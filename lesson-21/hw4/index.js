a002bd203f76746a33e1729ea02b7d040d07bf05
Nikita <shulgamalthael@gmail.com>
2020-08-14 12:55:02 +0300

    mod

--- lesson-21/hw4/index.js
+++ lesson-21/hw4/index.js
@@ -1,5 +1,11 @@
-export function setTitle(text) {
+function setTitle(text) {
     const setText = document.querySelector('.title');
     setText.textContent = text;
     // console.log(setText)
 };
+
+
+
+// setTitle('hg');
+
+export{ setTitle };
